9774d7ae5652476790b6d721212f6547f8196766
Pragun Goyal <pragun@mit.edu>
2012-10-20 21:53:21 -0400

    Added readsms and sendsms

---
+++ src/readsms.py
@@ -0,0 +1,58 @@
+import serial, sys, datetime
+
+typedict = {
+    'unread':'REC UNREAD',
+    'read' : 'REC READ',
+    'all' : 'ALL'
+}
+
+def sms(index,customer,time,date,msg):
+    a = {}
+    a['index'] = index.__str__()
+    a['customer'] = customer.__str__()
+    a['time'] = time.__str__()
+    a['date'] = date.__str__()
+    tmp1 = a['date'] + ' ' + a['time']
+    tmp2 = tmp1.split('-')[0]
+    a['timestamp'] = datetime.datetime.strptime(tmp2,'"%d/%m/%y %H:%M:%S')
+    a['contents'] = msg.__str__()
+    return a
+
+
+def readsms(port,a):
+    smslist = []
+    port = serial.Serial(port, 115200, timeout=5)
+    port.flushInput()
+    port.flushOutput()
+    port.write('AT\r\n')
+    #print 1,port.readline()
+    #print 2,port.readline()
+    port.write('AT+CMGF=1\r\n')
+    #print 1,port.readline()
+    #print 2,port.readline()
+    port.flushOutput()
+    port.flushInput()
+    port.write('AT+CMGL="%s"\r\n'%(typedict[a]))  #get all messages 
+    inside_cmgl_output = False
+    while(1):
+        line = port.readline()
+        print 'debug:',line
+        if line.startswith('AT+CMGL='):
+            inside_cmgl_output = True
+            continue
+        if line.startswith('+CMGL'):
+            info = line.split(',')
+            index = info[0].split(':')[1]
+            customer = info[2]
+            date = info[5]
+            time = info[4]
+            msg = port.readline()
+            print 'message#%s from %s date %s time %s %s'     \
+                %(index,customer,date,time,msg)
+            a = sms(index,customer,date,time,msg)
+            smslist.append(a)
+            gotmsg = True
+        if inside_cmgl_output and line.startswith('OK'):
+            break
+    return smslist
+
---
+++ src/sendsms.py
@@ -0,0 +1,34 @@
+import serial, sys
+
+def sendsms(port,text,number):
+    port = serial.Serial(port, 115200, timeout=2)
+    port.flushInput()
+    port.flushOutput()
+
+    #just send an AT
+    port.write('AT\r\n')
+    line = port.readline()
+    port.readline()
+
+    #set format
+    command = 'AT+CMGF=1\r\n'
+    port.write(command)
+    port.readline()
+    #port.readline()
+
+    #send send message command
+    command = 'AT+CMGS="%s"\rn'%(number.__str__())
+    #print command
+    port.write(command)
+    port.readline()
+    #port.readline()
+    
+    #send text
+    port.write(text)
+    
+    #send control-x
+    port.write('\x1A')
+    port.readline()
+    #port.readline()
+    #port.readline()
+
